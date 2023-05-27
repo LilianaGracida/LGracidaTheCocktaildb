44efbc72fb1f708b4449d90c39796e612ac0cd05
LilianaGracida <lgbrioso@gmail.com>
2023-05-26 18:50:54 -0600

    Add project files.

---
+++ ML/Cocktail.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ML
+{
+    public class Cocktail
+    {
+        public string Nombre { get; set; }
+        public string Ingrediente { get; set; }
+        public dynamic idDrink { get; set; }
+        public dynamic strDrink { get; set; }
+        public dynamic strDrinkAlternate { get; set; }
+        public dynamic strTags { get; set; }
+        public dynamic strVideo { get; set; }
+        public dynamic strCategory { get; set; }
+        public dynamic strIBA { get; set; }
+        public dynamic strAlcoholic { get; set; }
+        public dynamic strGlass { get; set; }
+        public dynamic strInstructions { get; set; }
+        public dynamic strInstructionsES { get; set; }
+        public dynamic strInstructionsDE { get; set; }
+        public dynamic strInstructionsFR { get; set; }
+        public dynamic strInstructionsIT { get; set; }
+        public dynamic strDrinkThumb { get; set; }
+        public dynamic strIngredient1 { get; set; }
+        public dynamic strIngredient2 { get; set; }
+        public dynamic strIngredient3 { get; set; }
+        public dynamic strIngredient4 { get; set; }
+        public dynamic strIngredient5 { get; set; }
+        public dynamic strIngredient6 { get; set; }
+        public dynamic strIngredient7 { get; set; }
+        public dynamic strIngredient8 { get; set; }
+        public dynamic strIngredient9 { get; set; }
+        public dynamic strIngredient10 { get; set; }
+        public dynamic strIngredient11 { get; set; }
+        public dynamic strIngredient12 { get; set; }
+        public dynamic strIngredient13 { get; set; }
+        public dynamic strIngredient14 { get; set; }
+        public dynamic strIngredient15 { get; set; }
+        public dynamic strMeasure1 { get; set; }
+        public dynamic strMeasure2 { get; set; }
+        public dynamic strMeasure3 { get; set; }
+        public dynamic strMeasure4 { get; set; }
+        public dynamic strMeasure5 { get; set; }
+        public dynamic strMeasure6 { get; set; }
+        public dynamic strMeasure7 { get; set; }
+        public dynamic strMeasure8 { get; set; }
+        public dynamic strMeasure9 { get; set; }
+        public dynamic strMeasure10 { get; set; }
+        public dynamic strMeasure11 { get; set; }
+        public dynamic strMeasure12 { get; set; }
+        public dynamic strMeasure13 { get; set; }
+        public dynamic strMeasure14 { get; set; }
+        public dynamic strMeasure15 { get; set; }
+        public dynamic strImageSource { get; set; }
+        public dynamic strImageAttribution { get; set; }
+        public dynamic strCreativeCommonsConfirmed { get; set; }
+        public dynamic dateModified { get; set; }
+
+        public List<Object> Productos { get; set; }
+    }
+}
---
+++ PL/Controllers/ThecocktaildbController.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Web;
+using System.Web.Mvc;
+
+namespace PL.Controllers
+{
+    public class ThecocktaildbController : Controller
+    {
+
+        public ActionResult GetCocktail()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult GetCocktail(ML.Cocktail cocktail)
+        {
+
+            ML.Result resultProducto = new ML.Result();
+            resultProducto.Objects = new List<Object>();
+
+            string s = cocktail.Nombre;
+            using (var client = new HttpClient())
+            {
+                client.BaseAddress = new Uri("https://www.thecocktaildb.com/api/json/");
+
+                var responseTask = client.GetAsync("v1/1/search.php?s=" + s);
+                responseTask.Wait(); 
+
+                var result = responseTask.Result;
+                if (result.IsSuccessStatusCode)
+                {
+                    var readTask = result.Content.ReadAsStringAsync();
+                    readTask.Wait();
+
+
+                    foreach (var resultItem in readTask.Result.)
+                    {
+                        ML.Cocktail resultItemList = Newtonsoft.Json.JsonConvert.DeserializeObject<ML.Cocktail>(resultItem.ToString());
+
+                        resultProducto.Objects.Add(resultItemList);
+                    }
+
+                }
+            }
+
+            return View(cocktail);
+        }
+    }
+}
